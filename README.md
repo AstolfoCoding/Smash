6158e06a83d09880d8b2b96cdc14a12bb0d5b276
tvaz <victoriavaz24@gmail.com>
2018-02-16 11:07:57 -0800

    Added database

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Smash
+
+#Install dependencies
+`pip install -r requirements.txt`
+
+#set FLASK_APP
+`export FLASK_APP=app.py`
+
+#set FLASK_DEBUG so you can see debug messages
+`export FLASK_DEBUG=1`
+
+#Run
+`flask run`
--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'smash.db'
+db = SQLAlchemy(app)
+
+class Character(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80),unique=True,nullable=False)
+    desc = db.Column(db.String(120),unique=True,nullable=False)
+
+    def __repr__(self):
+        return '<%r>' % self.name
 
 @app.route('/characters')
 def characters():
