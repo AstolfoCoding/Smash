68432589e5776f804c6d7d9a4f195ca2dfdc5bd2
tvaz <victoriavaz24@gmail.com>
2018-02-16 12:50:55 -0800

    Populated the database

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'smash.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./smash.db'
 db = SQLAlchemy(app)
 
 class Character(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(80),unique=True,nullable=False)
-    desc = db.Column(db.String(120),unique=True,nullable=False)
+    desc = db.Column(db.String(120),unique=False,nullable=True)
 
     def __repr__(self):
-        return '<%r>' % self.name
+        return '<Character: %r>' % self.name
 
 @app.route('/characters')
 def characters():
