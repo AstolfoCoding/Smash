6716b2456bbf309e0bed10da1fa7c26102c0f3b6
tvaz <victoriavaz24@gmail.com>
2018-02-20 15:50:07 -0800

    Fixed python script for style.

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./smash.db'
 db = SQLAlchemy(app)
 
+
 class Character(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(80),unique=True,nullable=False)
-    desc = db.Column(db.String(120),unique=False,nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    desc = db.Column(db.String(120), unique=False, nullable=True)
 
     def __repr__(self):
         return '<%r>' % self.name
 
+
 @app.route('/characters')
 def characters():
     return 'characters'
+
 
 @app.route('/characters/<character>')
 def character_name(character):
