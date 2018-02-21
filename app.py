f11b41cd03ff4f457dc5ff69cf1503f6ee42421c
Jonathan Velazquez <jvelaz07@uw.edu>
2018-02-21 11:31:54 -0800

    added ability to update character description using a post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, Blueprint, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    desc = db.Column(db.String(120), unique=False, nullable=True)
+    desc = db.Column(db.String(500), unique=False, nullable=True)
 
     def __repr__(self):
         return '<%r>' % self.name
@@ -21,14 +21,37 @@ character_blueprint = Blueprint('character', __name__, template_folder='template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    allChars = Character.query.all()
+    return render_template('index.html',list=allChars)
 
 
-@app.route('/characters/<character>')
-def characters(character):
-    return render_template('characters.html', value=character)
+@app.route('/character/<character>')
+def character(character):
+    nameInput = Character.query.filter_by(name=character).first()
+    desc = nameInput.desc
+    allChars = Character.query.all()
+    return render_template('character.html', name=character,description=desc, list=allChars)
+
+
+@app.route('/characters/')
+def characters():
+    allChars = Character.query.all()
+
+    return render_template('characters.html', list=allChars)
+
+@app.route('/rest/description/update', methods=['POST'])
+def updateDescription():
+    if request.method=='POST':
+        name = request.form['name']
+        desc = request.form['desc']
+        if desc:
+            query = Character.query.filter_by(name=name).first()
+            query.desc = desc
+            db.session.commit()
+            return query.desc
+        else:
+            return 'No description given'
 
 
 if __name__ == '__main__':
-    print(Character.query.filter_by(name='Mario').first())
     app.run()
